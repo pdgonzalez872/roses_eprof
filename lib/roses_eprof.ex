8acffdb3c01af2daf4135ffff0fc9523051b250f
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-01-31 08:43:07 -0600

    Formatter fixes

--- lib/roses_eprof.ex
+++ lib/roses_eprof.ex
@@ -2,13 +2,13 @@ defmodule RosesEprof do
   def testing_flow do
     # same as https://github.com/elixir-lang/flow example
     Path.join(["lib", "roses.txt"])
-    |> Path.absname
-    |> File.stream!
+    |> Path.absname()
+    |> File.stream!()
     |> Flow.from_enumerable()
     |> Flow.flat_map(&String.split(&1, " "))
     |> Flow.partition()
     |> Flow.reduce(fn -> %{} end, fn word, acc ->
-        Map.update(acc, word, 1, & &1 + 1)
+      Map.update(acc, word, 1, &(&1 + 1))
     end)
     |> Enum.to_list()
   end
