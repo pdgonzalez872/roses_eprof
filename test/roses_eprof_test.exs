fba54114539359f2532e082c52dbea965d502731
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-01-31 08:58:12 -0600

    Make tests simpler.

--- test/roses_eprof_test.exs
+++ test/roses_eprof_test.exs
@@ -7,18 +7,18 @@ defmodule RosesEprofTest do
   alias Mix.Tasks.Profile.Eprof
   alias Mix.Tasks.Profile.Fprof
 
-  test "returns an output for cprof" do
+  test "returns an output for cprof without erroring" do
     assert capture_io(fn -> Cprof.run(["-e", "RosesEprof.testing_flow"]) end) =~
-             ~r(Total                                                               5020)
+             ~r(CNT     \nTotal)
   end
 
-  test "returns an output for fprof" do
+  test "returns an output for fprof without erroring" do
     some_of_the_output = "CNT    ACC (ms)    OWN (ms)"
 
     assert capture_io(fn -> Fprof.run(["-e", "RosesEprof.testing_flow"]) end) =~ some_of_the_output
   end
 
-  test "returns an output for eprof" do
+  test "returns an output for eprof, errors" do
     some_of_the_output = "it fails here."
 
     assert capture_io(fn -> Eprof.run(["-e", "RosesEprof.testing_flow"]) end) =~ some_of_the_output
